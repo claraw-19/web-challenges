92222cc7bb7959cf1f29af51b6d1ec63459dcceb
Clara Weyer <clara.weyer@schullv.de>
2024-11-12 15:34:42 +0100

    change color

--- js-inputs-and-stings/js-inputs-and-strings_change-box-appearance/js/index.js
+++ js-inputs-and-stings/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -1,1 +1,15 @@
 console.clear();
+
+const inputColor = document.querySelector('[data-js="input-color"]');
+const inputRadius = document.querySelector('[data-js="input-radius"]');
+const inputRotation = document.querySelector('[data-js="input-rotation"]');
+const box = document.querySelector('[data-js="box"]');
+
+inputColor.addEventListener("input", () => {
+  const hue = inputColor.value;
+  box.style.backgroundColor = `hsl(${hue}, 50%, 50%)`;
+});
+
+inputRadius.addEventListener("click", () => {});
+
+inputRotation.addEventListener("click", () => {});
