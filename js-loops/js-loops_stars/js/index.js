14829d00d3eca7ca9c7b516f2e0f71f58e003e03
Clara Weyer <clara.weyer@schullv.de>
2024-11-14 14:56:21 +0100

    implement user input

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -7,16 +7,18 @@ function renderStars(filledStars) {
   starContainer.innerHTML = "";
 
   // --v-- write or modify code below this line --v--
-  for (i = 1; i <= 5; i++) {
+
+  for (let i = 1; i <= 5; i++) {
     const img = document.createElement("img");
-    if (i <= filledStars) {
-      img.src = "assets/star-filled.svg";
-    } else {
-      img.src = "assets/star-empty.svg";
-    }
+    const type = i <= filledStars ? "filled" : "empty";
+    // img.setAttribute("src", `assets/star-${type}.svg`);
+    img.src = `assets/star-${type}.svg`;
+    img.addEventListener("click", () => {
+      renderStars(i);
+    });
     starContainer.appendChild(img);
   }
   // --^-- write or modify code above this line --^--
 }
 
-renderStars(4);
+renderStars();
