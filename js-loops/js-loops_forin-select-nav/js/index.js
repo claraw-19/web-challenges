e210ebb45e023fee331b286db6d279b9c093e135
Clara Weyer <clara.weyer@schullv.de>
2024-11-14 12:09:12 +0100

    create select input

--- js-loops/js-loops_forin-select-nav/js/index.js
+++ js-loops/js-loops_forin-select-nav/js/index.js
@@ -22,6 +22,13 @@ main.append(select);
 
 // --v-- write or modify code below this line --v--
 
+for (const key in languages) {
+  const option = document.createElement("option");
+  option.value = key;
+  option.textContent = languages[key];
+  select.appendChild(option);
+}
+
 // --^-- write or modify code above this line --^--
 
 // Part 2: Creating a Navigation Bar
