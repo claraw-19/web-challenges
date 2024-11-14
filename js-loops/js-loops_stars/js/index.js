db006b97bdb21649772ca280cf65a376cc3bc917
Clara Weyer <clara.weyer@schullv.de>
2024-11-14 13:51:50 +0100

    change number of filled stars

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -2,17 +2,21 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // Reset the star container before re-rendering stars
   starContainer.innerHTML = "";
 
   // --v-- write or modify code below this line --v--
   for (i = 1; i <= 5; i++) {
     const img = document.createElement("img");
-    img.src = "assets/star-empty.svg";
+    if (i <= filledStars) {
+      img.src = "assets/star-filled.svg";
+    } else {
+      img.src = "assets/star-empty.svg";
+    }
     starContainer.appendChild(img);
   }
   // --^-- write or modify code above this line --^--
 }
 
-renderStars();
+renderStars(4);
