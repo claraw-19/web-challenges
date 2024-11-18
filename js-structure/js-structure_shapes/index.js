f9f2c5da2c335be97f1e6526d3a13ac3a1a5c5c5
Clara Weyer <clara.weyer@schullv.de>
2024-11-18 15:58:31 +0100

    create pentagon

--- js-structure/js-structure_shapes/index.js
+++ js-structure/js-structure_shapes/index.js
@@ -3,6 +3,7 @@ console.clear();
 import { getRandomColor } from "./utils/randomColor.js";
 import { Circle } from "./components/Circle/Circle.js";
 import { Square } from "./components/Square/Square.js";
+import { Pentagon } from "./components/Pentagon/Pentagon.js";
 
 const root = document.getElementById("root");
 
@@ -12,10 +13,7 @@ root.appendChild(circleElement);
 const squareElement = Square();
 root.appendChild(squareElement);
 
-const pentagon = document.createElement("div");
-pentagon.classList.add("pentagon");
-pentagon.addEventListener("click", () => {
-  pentagon.style.backgroundColor = getRandomColor();
-});
+const pentagonElement = Pentagon();
+root.appendChild(pentagonElement);
 
 root.append(circle, square, pentagon);
