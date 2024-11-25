3a9cdae166aa2e41ff90c6b2f37b42eef1882cba
Clara Weyer <clara.weyer@schullv.de>
2024-11-25 10:54:38 +0100

    add attributes in react

--- react-basics/react-basics_attributes/src/App.jsx
+++ react-basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,14 @@
 import "./styles.css";
 
 export default function App() {
-  return <article>Replace me with an article</article>;
+  return (
+    <article className="article">
+      <h2 className="aticle__title">Hello World!</h2>
+      <label htmlFor="userInput"></label>
+      <input id="userInput"></input>
+      <a className="article__link" href="https://de.wikipedia.org/wiki/React">
+        What ist React?
+      </a>
+    </article>
+  );
 }
