5a1d6ef2b725719f456bf24ecaaa3e08ebd04ee9
Clara Weyer <clara.weyer@schullv.de>
2024-11-25 16:14:33 +0100

    add click event and pass function as prop

--- react-props/react-props_button/src/App.jsx
+++ react-props/react-props_button/src/App.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import "./styles.css";
 
-function Button({ color, disabled, text }) {
+function Button({ color, disabled, text, onClick }) {
   return (
-    <button style={{ color: color }} disabled={disabled}>
+    <button style={{ color: color }} disabled={disabled} onClick={onClick}>
       {text}
     </button>
   );
 }
 
 export default function App() {
-  return <Button color={"blue"} disabled={false} text={"Click me!"} />;
+  const handleClick = () => {
+    console.log("You clicked me!");
+  };
+  return (
+    <Button
+      color={"blue"}
+      disabled={false}
+      text={"Click me!"}
+      onClick={handleClick}
+    />
+  );
 }
