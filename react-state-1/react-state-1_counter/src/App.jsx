41d2a2307fd8fad7078990803facc3b00a34b461
Clara Weyer <clara.weyer@schullv.de>
2024-11-26 15:30:17 +0100

    make buttons work

--- react-state-1/react-state-1_counter/src/App.jsx
+++ react-state-1/react-state-1_counter/src/App.jsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  let count = 0;
+  const [count, setCount] = useState(0);
 
   return (
     <div className="container">
@@ -10,15 +11,15 @@ export default function App() {
         <button
           type="button"
           onClick={() => {
-            console.log("🤔");
+            setCount(count - 1);
           }}
         >
-          -
+          -√
         </button>
         <button
           type="button"
           onClick={() => {
-            console.log("🤔");
+            setCount(count + 1);
           }}
         >
           +
