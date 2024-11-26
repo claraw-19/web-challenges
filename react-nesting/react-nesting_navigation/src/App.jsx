eae6809ce1e4b3469cab6eea57d01f1b28357824
Clara Weyer <clara.weyer@schullv.de>
2024-11-26 10:47:48 +0100

    outsource link component

--- react-nesting/react-nesting_navigation/src/App.jsx
+++ react-nesting/react-nesting_navigation/src/App.jsx
@@ -2,6 +2,7 @@ import "./styles.css";
 
 import { Header } from "./components/Header.jsx";
 import { Navigation } from "./components/Navigation.jsx";
+import { Link } from "./components/Link.jsx";
 
 import avatar from "./img/avatar.jpg";
 import logo from "./img/logo.jpg";
@@ -9,19 +10,13 @@ import logo from "./img/logo.jpg";
 export default function App() {
   return (
     <Header>
-      <a href="#">
+      <Link href="#">
         <img className="round-image" src={logo} alt="logo" />
-      </a>
+      </Link>
       <Navigation>
-        <a className="navigation__link" href="#home">
-          Home
-        </a>
-        <a className="navigation__link" href="#about">
-          About
-        </a>
-        <a className="navigation__link" href="#impressum">
-          Impressum
-        </a>
+        <Link href="#home">Home</Link>
+        <Link href="#about">About</Link>
+        <Link href="#impressum">Impressum</Link>
       </Navigation>
       <button
         type="button"
