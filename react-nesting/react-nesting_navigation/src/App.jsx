385e4fa6238fdc273c6bcbc6e5ce04d077e62c37
Clara Weyer <clara.weyer@schullv.de>
2024-11-26 10:31:21 +0100

    outsource header component

--- react-nesting/react-nesting_navigation/src/App.jsx
+++ react-nesting/react-nesting_navigation/src/App.jsx
@@ -1,35 +1,36 @@
 import "./styles.css";
 
+import { Header } from "./components/Header.jsx";
+
 import avatar from "./img/avatar.jpg";
 import logo from "./img/logo.jpg";
 
 export default function App() {
   return (
-    <>
-      <header className="header">
-        <a href="#">
-          <img className="round-image" src={logo} alt="logo" />
+    <Header>
+      <a href="#">
+        <img className="round-image" src={logo} alt="logo" />
+      </a>
+      <nav>
+        <a className="navigation__link" href="#home">
+          Home
         </a>
-        <nav>
-          <a className="navigation__link" href="#home">
-            Home
-          </a>
-          <a className="navigation__link" href="#about">
-            About
-          </a>
-          <a className="navigation__link" href="#impressum">
-            Impressum
-          </a>
-        </nav>
-        <button
-          type="button"
-          onClick={() => console.log("I could toggle a profile!")}
-          aria-label="toggle profile"
-        >
-          <img className="round-image" src={avatar} alt="avatar" />
-        </button>
-      </header>
+        <a className="navigation__link" href="#about">
+          About
+        </a>
+        <a className="navigation__link" href="#impressum">
+          Impressum
+        </a>
+      </nav>
+      <button
+        type="button"
+        onClick={() => console.log("I could toggle a profile!")}
+        aria-label="toggle profile"
+      >
+        <img className="round-image" src={avatar} alt="avatar" />
+      </button>
+
       <main>content goes here…</main>
-    </>
+    </Header>
   );
 }
