49c0ebd34c9500a703ab3f6cd086da81d150b9eb
Clara Weyer <clara.weyer@schullv.de>
2024-11-26 08:23:46 +0100

    optimize return

--- react-props/react-props_greeting/src/App.jsx
+++ react-props/react-props_greeting/src/App.jsx
@@ -3,9 +3,10 @@ import "./styles.css";
 function Greeting({ name }) {
   const coaches = ["Peter", "Klaus", "Esraa", "Andrea"];
   const isCoach = coaches.includes(name);
-  return <h1>{isCoach ? "Hello, Coach!" : `Hello, ${name}!`}</h1>;
+  // return <h1>{isCoach ? "Hello, Coach!" : `Hello, ${name}!`}</h1>;
+  return <h1>Hello {isCoach ? "Coach" : name}</h1>;
 }
 
 export default function App() {
-  return <Greeting name="Peter" />;
+  return <Greeting name="Clara" />;
 }
