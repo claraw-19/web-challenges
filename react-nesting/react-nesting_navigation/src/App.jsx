1812492a2bb76541b4416822922b4d74b06bb7fb
Clara Weyer <clara.weyer@schullv.de>
2024-11-26 11:03:00 +0100

    move main out of header

--- react-nesting/react-nesting_navigation/src/App.jsx
+++ react-nesting/react-nesting_navigation/src/App.jsx
@@ -10,24 +10,25 @@ import logo from "./img/logo.jpg";
 
 export default function App() {
   return (
-    <Header>
-      <Link href="#">
-        <Image src={logo} alt="logo" />
-      </Link>
-      <Navigation>
-        <Link href="#home">Home</Link>
-        <Link href="#about">About</Link>
-        <Link href="#impressum">Impressum</Link>
-      </Navigation>
-      <button
-        type="button"
-        onClick={() => console.log("I could toggle a profile!")}
-        aria-label="toggle profile"
-      >
-        <Image src={avatar} alt="avatar" />
-      </button>
-
+    <>
+      <Header>
+        <Link href="#">
+          <Image src={logo} alt="logo" />
+        </Link>
+        <Navigation>
+          <Link href="#home">Home</Link>
+          <Link href="#about">About</Link>
+          <Link href="#impressum">Impressum</Link>
+        </Navigation>
+        <button
+          type="button"
+          onClick={() => console.log("I could toggle a profile!")}
+          aria-label="toggle profile"
+        >
+          <Image src={avatar} alt="avatar" />
+        </button>
+      </Header>
       <main>content goes here…</main>
-    </Header>
+    </>
   );
 }
