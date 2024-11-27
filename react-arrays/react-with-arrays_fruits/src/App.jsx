2b85dc62400cb8bc2078e2876f40bc7485622c1b
Clara Weyer <clara.weyer@schullv.de>
2024-11-27 10:21:15 +0100

    create card for each fruit

--- react-arrays/react-with-arrays_fruits/src/App.jsx
+++ react-arrays/react-with-arrays_fruits/src/App.jsx
@@ -1,11 +1,19 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    { id: 1, name: "🍎 Apple", color: "red" },
+    { id: 2, name: "🍌 Banana", color: "yellow" },
+    { id: 3, name: "🍇 Grape", color: "purple" },
+    { id: 4, name: "🍊 Orange", color: "orange" },
+    { id: 5, name: "🍉 Watermelon", color: "green" },
+  ];
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map((fruit) => (
+        <Card key={fruit.id} name={fruit.name} />
+      ))}
     </div>
   );
 }
