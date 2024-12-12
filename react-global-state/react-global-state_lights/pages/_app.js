ca7efb5f87e3c4831135041a3ccdac84faed5d81
Clara Weyer <clara.weyer@schullv.de>
2024-12-12 14:10:05 +0100

    app works as before but with shared state

--- react-global-state/react-global-state_lights/pages/_app.js
+++ react-global-state/react-global-state_lights/pages/_app.js
@@ -1,13 +1,36 @@
 import GlobalStyle from "../styles";
 import Layout from "../components/Layout";
+import { useState } from "react";
+
+const initialLights = [
+  { id: 1, name: "Living Room", isOn: false },
+  { id: 2, name: "Kitchen", isOn: false },
+  { id: 3, name: "Bedroom", isOn: true },
+  { id: 4, name: "Bathroom", isOn: false },
+  { id: 5, name: "Garage", isOn: false },
+  { id: 6, name: "Porch", isOn: false },
+  { id: 7, name: "Garden", isOn: false },
+  { id: 8, name: "Office", isOn: false },
+];
 
 export default function App({ Component, pageProps }) {
-  const [isOn, setIsOn] = useState(false);
+  const [lights, setLights] = useState(initialLights);
+
+  function toggleLight(id) {
+    setLights((lights) =>
+      lights.map((light) => {
+        if (light.id === id) {
+          return { ...light, isOn: !light.isOn };
+        }
+        return light;
+      })
+    );
+  }
 
   return (
     <Layout>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component {...pageProps} lights={lights} toggleLight={toggleLight} />
     </Layout>
   );
 }
