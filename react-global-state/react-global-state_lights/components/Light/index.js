912b21b8d51961166e35d7a047cd421c3cf5fd3a
Clara Weyer <clara.weyer@schullv.de>
2024-12-12 12:23:06 +0100

    lift state isOn up from Light to _app

--- react-global-state/react-global-state_lights/components/Light/index.js
+++ react-global-state/react-global-state_lights/components/Light/index.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { LightButton, Icon, Text, Name, State } from "./Light.styled";
 
 export default function Light({ name }) {
-  const [isOn, setIsOn] = useState(false);
+  // const [isOn, setIsOn] = useState(false);
 
   function handleToggle() {
     setIsOn((isOn) => !isOn);
--- react-global-state/react-global-state_lights/pages/_app.js
+++ react-global-state/react-global-state_lights/pages/_app.js
@@ -2,6 +2,8 @@ import GlobalStyle from "../styles";
 import Layout from "../components/Layout";
 
 export default function App({ Component, pageProps }) {
+  const [isOn, setIsOn] = useState(false);
+
   return (
     <Layout>
       <GlobalStyle />
