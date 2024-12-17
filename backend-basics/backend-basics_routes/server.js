4d60c7e87d34c0dbb7ec1fae0658b26455e572f5
Clara Weyer <clara.weyer@schullv.de>
2024-12-17 11:39:49 +0100

    Backend Basics Routes

--- backend-basics/backend-basics_routes/server.js
+++ backend-basics/backend-basics_routes/server.js
@@ -1,3 +1,14 @@
 import { createServer } from "node:http";
 
-// export const server = …
+export const server = createServer((request, response) => {
+  if (request.url == "/api/fish/1") {
+    response.end("Shrimp");
+    response.statusCode = 200;
+  } else if (request.url == "/api/fish/2") {
+    response.end("Anemonefish");
+    response.statusCode = 200;
+  } else {
+    response.end("Not found");
+    response.statusCode = 404;
+  }
+});
